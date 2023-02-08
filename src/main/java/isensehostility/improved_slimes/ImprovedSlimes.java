8bc900cd25af57f312a6846df328aca37267522b
ISenseHostility <yaboijarno@gmail.com>
2023-02-08 22:58:15 +0100

    Made all values configurable

--- src/main/java/isensehostility/improved_slimes/ImprovedSlimes.java
+++ src/main/java/isensehostility/improved_slimes/ImprovedSlimes.java
@@ -1,8 +1,11 @@
 package isensehostility.improved_slimes;
 
+import isensehostility.improved_slimes.config.ISConfig;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 @Mod(ImprovedSlimes.MODID)
 public class ImprovedSlimes {
@@ -10,7 +13,7 @@ public class ImprovedSlimes {
     public static final String MODID = "improved_slimes";
 
     public ImprovedSlimes() {
-
+        ISConfig.loadConfig(ISConfig.config, FMLPaths.CONFIGDIR.get().resolve(MODID + "-config.toml").toString());
     }
 
     public static float setMaxHealth(LivingEntity entity, float health) {
@@ -19,5 +22,9 @@ public class ImprovedSlimes {
         float maxHealth = entity.getMaxHealth();
         entity.heal(maxHealth);
         return maxHealth;
+    }
+
+    public static boolean isSourceWeakness(DamageSource source) {
+        return source == DamageSource.MAGIC || source == DamageSource.ON_FIRE || source == DamageSource.LAVA || source == DamageSource.IN_FIRE;
     }
 }
